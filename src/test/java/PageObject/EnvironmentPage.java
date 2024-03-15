abdb1f8caeb841afa1adde8a925ab54c182ffaf7
Ilya Poplavsky <i.poplavsky@paynocchio.com>
2024-03-15 11:27:41 +0300

    Construction added for EnvironmentPage and RewardingPage

--- src/test/java/PageObject/EnvironmentPage.java
+++ src/test/java/PageObject/EnvironmentPage.java
@@ -1,9 +1,15 @@
 package PageObject;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class EnvironmentPage {
     @FindBy(css = "button[type='button']")
     private WebElement newEnvironment;
+    public EnvironmentPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
 }
+
--- src/test/java/PageObject/RewardingPage.java
+++ src/test/java/PageObject/RewardingPage.java
@@ -1,9 +1,15 @@
 package PageObject;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class RewardingPage {
     @FindBy(css = "button[type='button']")
     private WebElement newRewardingCampaign;
+    public RewardingPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
 }
+
