53f43b61960e182e58f137dad9d3d722cafef043
Ilya Poplavsky <i.poplavsky@paynocchio.com>
2024-03-20 17:36:20 +0300

    Logout test added

--- src/test/java/ApiTests/ApiTestAuthorization.java
+++ src/test/java/ApiTests/ApiTestAuthorization.java
@@ -25,7 +25,6 @@ public class ApiTestAuthorization {
     }
     @Test
     @DisplayName("Verify that login is succeed")
-    @Step ("Put requests")
     public void authorizationByPostApi() {
         String email = "i.poplavsky+29@paynocchio.com";
         String password = "Qwerye1!2345";
@@ -45,7 +44,6 @@ public class ApiTestAuthorization {
     }
     @Test
     @DisplayName("Input wrong password during login")
-    @Step ("Put requests")
     public void invalidAuthorizationByPostApi() {
         String email = "i.poplavsky+29@paynocchio.com";
         String password = "Qwerye1!0988";
@@ -62,6 +60,33 @@ public class ApiTestAuthorization {
                 .assertThat()
                 .statusCode(403)
                 .body("detail", equalTo("Could not validate credentials"));
+    }
+    //TODO Fix 404 not found "Expected status code <204> but was <404>."
+    @Test
+    @DisplayName("Logout after authorization check")
+    public void logOutAfterAuthorization() {
+        String email = "i.poplavsky+29@paynocchio.com";
+        String password = "Qwerye1!2345";
+        Map<String, String> loginRequest = new HashMap<>();
+        loginRequest.put("email", email);
+        loginRequest.put("password", password);
+        loginRequest.put("device", "desktop");
+        given().contentType("application/json")
+                .body(loginRequest)
+                .when()
+                .post(baseURI + "user/auth/login")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("access_token", is(notNullValue()));
+        given().contentType("application/json")
+                .when()
+                .post(baseURI + "/user/auth/logout")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(204);
     }
 }
 
